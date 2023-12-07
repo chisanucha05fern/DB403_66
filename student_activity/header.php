ab666bf2ccd95641193001cfbc0f3b722c073698
chisanucha05fern <114204921+chisanucha05fern@users.noreply.github.com>
2023-12-08 01:35:52 +0700

    Add files via upload

--- student_activity/header.php
+++ student_activity/header.php
@@ -13,12 +13,13 @@ require 'connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Student Activity</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   </head>
   <body>
   <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="./index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <img src="img/one piece.png" alt="" width="200" height="">
+      <img src="images/Pringles-logo.png" alt="" width="200" height="">
         <span class="fs-4">Simple header</span>
       </a>
 
@@ -28,7 +29,22 @@ require 'connect.php';
       </ul>
       <div class="dropdown text-end ms-3">
           <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+      <?php
+      $sql = "SELECT image FROM student WHERE studentID='{$_SESSION['user']['studentID']}'";
+      $result=$conn->query($sql);
+      $image = $result->fetch_assoc()['image'];
+      if($image) {
+        echo"<img src='images/profile/$image'
+        class='rounded-circle' height='42px'>" ;
+      }
+      else {
+          echo '<span class="material-symbols-outlined"
+          style="font-size:42px;">
+          face_2
+</span>';
+      }
+      ?>
+          
           </a>
           <ul class="dropdown-menu text-small" style="">
             <li><div class="dropdown-item text-primary"><?php echo $_SESSION['user']['studentName'];?></div></li>
